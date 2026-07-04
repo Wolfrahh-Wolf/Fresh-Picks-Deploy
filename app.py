91a9418a49551f7d75a778c0a9cd92ca6ffabb46
Yashwanth A <yashwanth2025offl@gmail.com>
2026-07-04 12:30:21 +0530

    Fix: Sneder Name in Mail

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def _send_otp_email(recipient_email, otp_code, purpose, reference=""):
     try:
         subject, text_body, html_body = _otp_email_html(purpose, otp_code, reference)
         message = SGMail(
-            from_email   = Config.SMTP_EMAIL,
+            from_email = ("FreshPicks", Config.SMTP_EMAIL),
             to_emails    = recipient_email,
             subject      = subject,
             html_content = html_body
@@ -1600,7 +1600,7 @@ def api_email_receipt(order_id):
             pdf_data = base64.b64encode(f.read()).decode()
 
         message = SGMail(
-            from_email   = Config.SMTP_EMAIL,
+            from_email = ("FreshPicks", Config.SMTP_EMAIL),
             to_emails    = user_email,
             subject      = f"FreshPicks Receipt — {order_id}",
             html_content = f"<p>Dear {receipt_data['full_name']},</p><p>Please find your FreshPicks receipt for order <strong>{order_id}</strong> attached.</p><p>Thank you for shopping with us!</p><p><strong>FreshPicks</strong></p>"
